42705b632e9530fffb85deec76872440dc7b3b5f
SunjanaK <sunjana101@gmail.com>
2018-09-22 11:21:09 -0400

    Letters in word being spoken

--- ListPrinter.py
+++ ListPrinter.py
@@ -1,34 +1,113 @@
-#
-# from tkinter import *
-#
-# def draw(canvas, width, height):
-
-#     for word in listy:
-#         for letter in word:
-#             canvas.create_text(200, 100, text=letter,
-#                         font="Helvetica 26 bold underline")
-#
-# def runDrawing(width=300, height=300):
-#     root = Tk()
-#     root.resizable(width=False, height=False) # prevents resizing window
-#     canvas = Canvas(root, width=width, height=height)
-#     canvas.configure(bd=0, highlightthickness=0)
-#     canvas.pack()
-#     draw(canvas, width, height)
-#     root.mainloop()
-#
-#
-# runDrawing(400, 200)
 
 
 from tkinter import *  # from tkinter import *
+import time
 
-listy = ["cat"]
+from google.cloud import texttospeech
+client = texttospeech.TextToSpeechClient()
 
-root = Tk()
-t = Text(root)
+
+listy = ["I", "WANT", "CAKE"]
+import pygame
+#
+# root = Tk()
+# t = Text(root)
+#
+i = 0
+# t.pack()
+
+
+def loadmp3player(file):
+        # print(file)
+        pygame.init()
+        pygame.mixer.init()
+        pygame.mixer.music.load(file)
+        pygame.mixer.music.play()
+        time.sleep(0.5)
+        # pygame.event.wait()
+
 for word in listy:
+    # t.insert(END, word + " ")
+    synthesis_input = texttospeech.types.SynthesisInput(text="What does")
+
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    with open('output1.mp3', 'wb') as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        # print('Audio content written to file "output1.mp3"')
+        out.close()
+    file = 'output1.mp3'
+    loadmp3player(file)
+    # pygame.init()
+    # pygame.mixer.init()
+    # pygame.mixer.music.load(file)
+    # pygame.mixer.music.play()
+    # pygame.event.wait()
     for letter in word:
-        t.insert(END, letter + " ")
-t.pack()
-root.mainloop()
+        # time.sleep(0.5)
+        print(letter)
+        synthesis_input = texttospeech.types.SynthesisInput(text=letter)
+        voice = texttospeech.types.VoiceSelectionParams(
+            language_code='en-US',
+            ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+        audio_config = texttospeech.types.AudioConfig(
+            audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+        response = client.synthesize_speech(synthesis_input, voice, audio_config)
+        with open(str(i)+ ".mp3", 'wb') as out:
+            # Write the response to the output file.
+            out.write(response.audio_content)
+            # print('Audio content written to file "output2.mp3"')
+            out.close()
+        file = str(i)+ ".mp3"
+        loadmp3player(file)
+        i += 1
+
+        # pygame.init()
+        # pygame.mixer.init()
+        # pygame.mixer.music.load(file)
+        # pygame.mixer.music.play()
+        # pygame.event.wait()
+
+    synthesis_input = texttospeech.types.SynthesisInput(text="spell")
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    with open('output3.mp3', 'wb') as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        # print('Audio content written to file "output.mp3"')
+        out.close()
+    file = 'output3.mp3'
+    loadmp3player(file)
+    # pygame.init()
+    # pygame.mixer.init()
+    # pygame.mixer.music.load(file)
+    # pygame.mixer.music.play()
+    # pygame.event.wait()
+
+
+# root.mainloop()
+
+
+# loadmp3player()
+# print('som')
+
+#
+#
+# with open('output.mp3', 'wb') as out:
+#     # Write the response to the output file.
+#     out.write(response.audio_content)
+#     print('Audio content written to file "output.mp3"')
+# pygame.init()
+# pygame.mixer.init()
+# pygame.mixer.music.load(file)
+# pygame.mixer.music.play()
+# pygame.event.wait()
